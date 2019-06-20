cfb541481bb52dcc810cb8b1008cfc878abed2a2
Bojan Markovic <bolom992@gmail.com>
2019-06-20 19:08:58 +0200

    Initial commit.

---
+++ CosmicDevTestApp/CosmicDevTestApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  CosmicDevTestApp
+//
+//  Created by Bojan Markovic on 20/06/2019.
+//  Copyright © 2019 Bojan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
